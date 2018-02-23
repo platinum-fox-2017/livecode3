559cc2a389f3986c433270ded808f4ada23e5861
kevinhimawanhie <kevinhimawanhie@gmail.com>
2018-02-23 10:59:05 +0700

    LiveCode3 - Feedback

--- async/index.js
+++ async/index.js
@@ -17,6 +17,73 @@ class RNG {
 
     return result;
   }
+
+  static gatchaRoll(times,callback){
+    let biggest = 0
+
+    if(times !== 0){
+      for(let i = 0; i < times; i++){
+        let result = RNG.roll()
+        if(result > biggest){
+          biggest = result
+        }
+      }
+    }
+
+    setTimeout(function() {
+      callback(biggest)
+    }, 1000);
+  }
+
+  static gatchaRollPromise(times){
+    return new Promise((resolve,reject)=>{
+      let biggest = 0
+      if(times !== 0){
+        for(let i = 0; i < times; i++){
+          let result = RNG.roll()
+          if(result > biggest){
+            biggest = result
+          }
+        }
+      }
+  
+      if(biggest > 0){
+        setTimeout(function() {
+          resolve(biggest)
+        }, 1000);
+      }else{
+        setTimeout(function() {
+          reject()
+        }, 1000);
+      }
+    })
+  }
+
+  // static gatchaRollPromise2(times,repitision){
+  //   for(let i = 0; i < repitision; i++){
+  //     return new Promise ((resolve,reject)=>{
+  //       let biggest = 0
+  //       if(times !== 0){
+  //         for(let j = 0; j < times; j++){
+  //           let result = RNG.roll()
+  //           if(result > biggest){
+  //             biggest = result
+  //           }
+  //         }
+  //       }
+
+  //       if(biggest > 0){
+  //         setTimeout(function() {
+  //           resolve(biggest)
+  //         }, 1000);
+  //       }else{
+  //         setTimeout(function() {
+  //           reject('err')
+  //         }, 1000);
+  //       }
+  //     })
+  //   }
+  // }
 }
 
 function viewGachaResult(best) {
@@ -33,19 +100,54 @@ RNG.gatchaRoll(1, function(result) { viewGachaResult(result) }); // output log s
 RNG.gatchaRoll(0, function(result) { viewGachaResult(result) }); // output: 0
 
 // RELEASE 1 TEST CASES
-RNG.gatchaRollPromise(5)
-  .then(function(result) { viewGachaResult(result) })
-  .catch(function(err) { viewGachaFailure() };
+
+RNG.gatchaRollPromise(5).then(function (result){
+  viewGachaResult(result)
+}).catch(function(err){viewGachaFailure()})
 
 // akan menampilkan di log: YOUR BEST GATCHA ROLL RESULT IS <angka antara 1-5>
 
-RNG.gatchaRollPromise(0)
-  .then(function(result) { viewGachaResult(result) })
-  .catch(function(err) { viewGachaFailure() };
+RNG.gatchaRollPromise(0).then(function(result) {
+   viewGachaResult(result) 
+}).catch(function(err) {
+   viewGachaFailure() });
 
 // akan menampilkan di log: YAKIN NGGAK MAU NGE-ROLL?
 
 
 // RELEASE 2 PROMISE(S)
 
-// code here...
+
+RNG.gatchaRollPromise(2).then((result)=>{
+     viewGachaResult(result) 
+     return RNG.gatchaRollPromise(result)
+}).then((result2)=>{
+  viewGachaResult(result2) 
+  return RNG.gatchaRollPromise(result2)
+}).then((result3)=>{
+  viewGachaResult(result3)
+  return RNG.gatchaRollPromise(result3)
+}).then((result4)=>{
+  viewGachaResult(result4)
+  return RNG.gatchaRollPromise(result4)
+}).then((result5)=>{
+  viewGachaResult(result5)
+  return RNG.gatchaRollPromise(result5)
+}).then((result6)=>{
+  viewGachaResult(result6)
+  return RNG.gatchaRollPromise(result6)
+}).then((result7)=>{
+  viewGachaResult(result7)
+  return RNG.gatchaRollPromise(result7)
+}).then((result8)=>{
+  viewGachaResult(result8)
+  return RNG.gatchaRollPromise(result8)
+}).then((result9)=>{
+  viewGachaResult(result9)
+  return RNG.gatchaRollPromise(result9)
+}).then((result10)=>{
+  viewGachaResult(result10)
+  return RNG.gatchaRollPromise(result10)
+}).catch((err)=>{
+     viewGachaFailure()
+})
