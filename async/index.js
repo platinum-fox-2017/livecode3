6035978283da40ab367a957099caf703f18de523
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-23 09:34:36 +0700

    finished async

--- async/index.js
+++ async/index.js
@@ -17,7 +17,40 @@ class RNG {
 
     return result;
   }
+
+  static gatchaRoll(times, callback){
+      let maxVal = 0;
+
+      for(let i = 0; i < times; i++){
+          let val = RNG.roll();
+          if (val > maxVal)
+            maxVal = val;
+      }
+      setTimeout(function(){callback(maxVal)}, 1000);
+  }
+
+
+  static gatchaRollPromise (times){
+      return new Promise((resolve,reject)=>{
+        if(times == 0){
+            setTimeout(function(){reject()}, 1000);
+        }
+        else{
+            let maxVal = 0;
+
+            for(let i = 0; i < times; i++){
+                let val = RNG.roll();
+                if (val > maxVal)
+                  maxVal = val;
+            }
+            setTimeout(function(){resolve(maxVal)}, 1000);
+        }
+
+      })
+  }
+
 }
+
 
 function viewGachaResult(best) {
   console.log(`YOUR BEST GATCHA ROLL RESULT IS ${best}`);
@@ -28,6 +61,7 @@ function viewGachaFailure() {
 }
 
 // RELEASE 0 TEST CASES
+
 RNG.gatchaRoll(5, function(result) { viewGachaResult(result) }); // output log sesuai hasil random terbaik
 RNG.gatchaRoll(1, function(result) { viewGachaResult(result) }); // output log sesuai hasil random terbaik
 RNG.gatchaRoll(0, function(result) { viewGachaResult(result) }); // output: 0
@@ -35,17 +69,58 @@ RNG.gatchaRoll(0, function(result) { viewGachaResult(result) }); // output: 0
 // RELEASE 1 TEST CASES
 RNG.gatchaRollPromise(5)
   .then(function(result) { viewGachaResult(result) })
-  .catch(function(err) { viewGachaFailure() };
+  .catch(function(err) { viewGachaFailure() });
 
 // akan menampilkan di log: YOUR BEST GATCHA ROLL RESULT IS <angka antara 1-5>
 
 RNG.gatchaRollPromise(0)
   .then(function(result) { viewGachaResult(result) })
-  .catch(function(err) { viewGachaFailure() };
+  .catch(function(err) { viewGachaFailure() });
 
 // akan menampilkan di log: YAKIN NGGAK MAU NGE-ROLL?
 
 
 // RELEASE 2 PROMISE(S)
-
+RNG.gatchaRollPromise(5)
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .then(function(result) {
+      viewGachaResult(result);
+      return RNG.gatchaRollPromise(5);
+  })
+  .catch(function(err) { viewGachaFailure() });
 // code here...
