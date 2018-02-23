9452f7b1df5e5c35ebf8d77b6b4671a83291e402
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-23 10:50:52 +0700

    Add cli-table

--- query/display.js
+++ query/display.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database.db');
+const Table = require('cli-table');
 
 //RELEASE 0
 let sql =  `SELECT s.title, s.point, s.level, p.name, p.age, p.gender
@@ -22,5 +23,28 @@ let sql2 = `SELECT count(*) as totalCards, p.id as playerId, p.name, p.age, p.ge
             LIMIT 3`
 db.all(sql2, [], (err, rows) => {
     if(err) console.log(err);
-    console.log(rows);
+    console.log(displayTable(rows));
 })
+
+function displayTable(data){
+    var table = new Table({
+        chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+               , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+               , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+               , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+      });
+     
+    for(let i=0; i<data.length; i++){
+        if(i==0){
+            table.push(
+                ['totalCards', 'playerId', 'name', 'age', 'gender'],
+                [data[i].totalCards, data[i].playerId, data[i].name, data[i].age, data[i].gender]
+            ); 
+        }else{
+            table.push(
+                [data[i].totalCards, data[i].playerId, data[i].name, data[i].age, data[i].gender]
+            ); 
+        }
+    }
+    return table.toString();
+}
