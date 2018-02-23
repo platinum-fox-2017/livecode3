72fba740531aab877d01116238b7ffc3d236383d
agrha <agrhakartz@gmail.com>
2018-02-23 10:51:46 +0700

    tambah query release 1

--- async/index.js
+++ async/index.js
@@ -36,7 +36,37 @@ class RNG {
   }
 
   static gatchaRollPromise(times){
-    
+    return New Promise({
+        new Promise((resolve, reject) => {
+          const now = Date.now();
+          const then = now + seconds * 1000;
+          let count = setInterval(() => {
+            var secondsLeft = Math.ceil(((then - Date.now()) / 1000)+1);
+            let minutes = Math.floor(secondsLeft/60)
+            let detik = secondsLeft%60
+            if (secondsLeft < 0) {
+              resolve('SIYAAAP');
+              clearInterval(count);
+              displayCountdown('BOOM')
+            } else if(minutes < 10 && detik <10){
+              displayCountdown('0'+minutes+':0'+detik)
+              secondsLeft -= 1
+            } else if (minutes < 10){
+              displayCountdown('0'+minutes+':'+detik)
+              secondsLeft -= 1
+            } else if (detik <10){
+              displayCountdown(minutes+':0'+detik)
+              secondsLeft -= 1
+            } else {
+              displayCountdown(minutes+':'+detik)
+              secondsLeft -= 1
+            }
+          }, 1000);
+        });
+
+    })
+  }
+
   }
 }
 
