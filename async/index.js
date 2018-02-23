c8a84166c58c32d03ac18eddef89336cacb01800
agrha <agrhakartz@gmail.com>
2018-02-23 10:30:06 +0700

    tambah gacha release 0

--- async/index.js
+++ async/index.js
@@ -2,7 +2,7 @@ class RNG {
   static roll() {
     const tiers = [1, 2, 3, 4, 5];
     let num = Math.floor(Math.random() * 100) + 1;
-    let result = 'UNKNOWN';
+    let result = 0;
     if(num > 60) {
       result = tiers[0];
     } else if(num > 40) {
@@ -14,10 +14,33 @@ class RNG {
     } else {
       result = tiers[4];
     }
+    return result
+}
 
-    return result;
+  static gatchaRoll(times,callback){
+      let best = 0
+      var counter = setInterval(function(){
+          if(times === 0){
+            callback(best)
+            clearInterval(counter)
+          } else {
+            let a = RNG.roll()
+            console.log(`dapet kocokan ${a}`);
+            if(a > best) {
+              best = a
+            }
+            times -= 1
+          }
+        },1000)
+    return ''
+  }
+
+  static gatchaRollPromise(times){
+    
   }
 }
+
+
 
 function viewGachaResult(best) {
   console.log(`YOUR BEST GATCHA ROLL RESULT IS ${best}`);
@@ -29,19 +52,19 @@ function viewGachaFailure() {
 
 // RELEASE 0 TEST CASES
 RNG.gatchaRoll(5, function(result) { viewGachaResult(result) }); // output log sesuai hasil random terbaik
-RNG.gatchaRoll(1, function(result) { viewGachaResult(result) }); // output log sesuai hasil random terbaik
-RNG.gatchaRoll(0, function(result) { viewGachaResult(result) }); // output: 0
+// RNG.gatchaRoll(1, function(result) { viewGachaResult(result) }); // output log sesuai hasil random terbaik
+// RNG.gatchaRoll(0, function(result) { viewGachaResult(result) }); // output: 0
 
 // RELEASE 1 TEST CASES
-RNG.gatchaRollPromise(5)
-  .then(function(result) { viewGachaResult(result) })
-  .catch(function(err) { viewGachaFailure() };
+// RNG.gatchaRollPromise(5)
+//   .then(function(result) { viewGachaResult(result) })
+//   .catch(function(err) { viewGachaFailure() });
 
 // akan menampilkan di log: YOUR BEST GATCHA ROLL RESULT IS <angka antara 1-5>
 
-RNG.gatchaRollPromise(0)
-  .then(function(result) { viewGachaResult(result) })
-  .catch(function(err) { viewGachaFailure() };
+// RNG.gatchaRollPromise(0)
+//   .then(function(result) { viewGachaResult(result) })
+//   .catch(function(err) { viewGachaFailure() };
 
 // akan menampilkan di log: YAKIN NGGAK MAU NGE-ROLL?
 
