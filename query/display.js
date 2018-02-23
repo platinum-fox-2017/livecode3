eb62441f6092cd4d1bbbeafeb347319557726287
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-23 10:04:20 +0700

    Release 1 Query

--- query/display.js
+++ query/display.js
@@ -4,10 +4,16 @@ const db = new sqlite3.Database('query/database.db');
 let query = "SELECT Status.title, Status.point, Status.level, Players.name,Players.age , Players.gender  ";
 query += "FROM Players INNER JOIN Status ON  Status.playerId = Players.id ";
 query += "GROUP BY Status.title ORDER BY Status.level DESC ";
-console.log(query);
 db.all(query, function(err, rows) {
   console.log(err);
   console.log(rows);
 });
-
+query = "SELECT  COUNT(*)  as totalCard, Cards.playerId, Players.name, Players.age,  Players.gender ";
+query += "FROM Players INNER JOIN Cards ON Cards.playerId = Players.id ";
+query += "WHERE Players.gender = 'Male' ";
+query += "GROUP BY Cards.playerId ORDER BY totalCard DESC LIMIT 3";
+db.all(query, function(err, rows) {
+  console.log(err);
+  console.log(rows);
+});
 db.close();
